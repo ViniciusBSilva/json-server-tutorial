59cf0441ca13dde1f8dbd5145453be85d9950e6e
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2023-05-02 23:46:05 -0300

    details.js implementation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const container = document.querySelector('.blogs');
 
 const renderPosts = async () => {
-    let uri = 'http://localhost:3000/posts';
+    const uri = 'http://localhost:3000/posts';
 
     const res = await fetch(uri);
 
@@ -12,13 +12,13 @@ const renderPosts = async () => {
     let template = '';
     posts.forEach(post => {
         template += `
-        <div class="post">
-            <h2>${post.title}</h2>
-            <p><small>${post.likes} likes</small></p>
-            <p>${post.body.slice(0, 200)}</p>
-            <a href="/details.html">read more...</a>
-        </div>
-    `
+                <div class="post">
+                    <h2>${post.title}</h2>
+                    <p><small>${post.likes} likes</small></p>
+                    <p>${post.body.slice(0, 200)}</p>
+                    <a href="/details.html?id=${post.id}">read more...</a>
+                </div>
+            `
     });
 
     container.innerHTML = template;
